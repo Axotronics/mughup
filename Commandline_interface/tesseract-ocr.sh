6f2701bc9351e0dcc656394893bc1f22607a35ad
Akshay V.R <64145832+akshayvr-programmer@users.noreply.github.com>
2021-03-13 23:13:23 +0530

    Add files via upload

---
+++ Commandline_interface/tesseract-ocr.sh
@@ -0,0 +1,16 @@
+aksha@DESKTOP-U4VIJ84 MINGW64 ~ (master)
+$ cd Desktop
+
+aksha@DESKTOP-U4VIJ84 MINGW64 ~/Desktop (master)
+$ tesseract img_2 'test_file'
+Tesseract Open Source OCR Engine v5.0.0-alpha.20201127 with Leptonica
+Error, cannot read input file img_2: No such file or directory
+Error during processing.
+
+aksha@DESKTOP-U4VIJ84 MINGW64 ~/Desktop (master)
+$ tesseract img_2.jpg 'test_file'
+Tesseract Open Source OCR Engine v5.0.0-alpha.20201127 with Leptonica
+Invalid SOS parameters for sequential JPEG
+
+aksha@DESKTOP-U4VIJ84 MINGW64 ~/Desktop (master)
+$
